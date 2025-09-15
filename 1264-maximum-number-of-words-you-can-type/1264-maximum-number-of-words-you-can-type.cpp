ec24e2e051315eccd4d65b5441fc916f243d0a90
Unaiza Nouman <145625176+unaizanouman@users.noreply.github.com>
2025-09-15 15:13:33 +0500

    Time: 0 ms (100%), Space: 9.4 MB (23.59%) - LeetHub

---
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {
+        unordered_set<char> broken(brokenLetters.begin(), brokenLetters.end());
+        int count = 0;
+        string word;
+        stringstream ss(text);
+
+        while (ss >> word) {
+            bool canType = true;
+            for (char c : word) {
+                if (broken.count(c)) {
+                    canType = false;
+                    break;
+                }
+            }
+            if (canType) count++;
+        }
+        return count;
+    }
+};
