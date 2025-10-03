99a4848e1774741799161538b6c319a5b157c651
Unaiza Nouman <145625176+unaizanouman@users.noreply.github.com>
2025-10-03 17:24:57 +0500

    Time: 93 ms (25.78%), Space: 28.9 MB (24.13%) - LeetHub

---
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int trapRainWater(vector<vector<int>>& heightMap) {
+        int m = heightMap.size();
+        int n = heightMap[0].size();
+        if (m <= 2 || n <= 2) return 0; // no water can be trapped
+
+        // Min-heap {height, row, col}
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+
+        // Push all boundary cells into heap
+        for (int i = 0; i < m; i++) {
+            pq.push({heightMap[i][0], i, 0});
+            pq.push({heightMap[i][n-1], i, n-1});
+            visited[i][0] = visited[i][n-1] = true;
+        }
+        for (int j = 1; j < n-1; j++) {
+            pq.push({heightMap[0][j], 0, j});
+            pq.push({heightMap[m-1][j], m-1, j});
+            visited[0][j] = visited[m-1][j] = true;
+        }
+
+        int water = 0;
+        vector<int> dirs = {0,1,0,-1,0};
+
+        while (!pq.empty()) {
+            auto top = pq.top(); pq.pop();
+            int h = top[0], r = top[1], c = top[2];
+
+            // check 4 directions
+            for (int k = 0; k < 4; k++) {
+                int nr = r + dirs[k], nc = c + dirs[k+1];
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && !visited[nr][nc]) {
+                    visited[nr][nc] = true;
+                    // If neighbor lower → water trapped
+                    if (heightMap[nr][nc] < h) {
+                        water += h - heightMap[nr][nc];
+                    }
+                    // Push with max boundary
+                    pq.push({max(h, heightMap[nr][nc]), nr, nc});
+                }
+            }
+        }
+
+        return water;
+    }
+};
