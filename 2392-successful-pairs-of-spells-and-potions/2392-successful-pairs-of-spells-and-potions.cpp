ad7402333636c32aecc9ae4b49f4154039e29c46
Unaiza Nouman <145625176+unaizanouman@users.noreply.github.com>
2025-10-08 18:15:58 +0500

    Time: 42 ms (67.49%), Space: 128.3 MB (42.11%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
@@ -1,14 +1,14 @@
-class Solution {
+class Solution{
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
         sort(potions.begin(), potions.end());
         vector<int> ans;
-        int m = potions.size();
+        int m=potions.size();
         
-        for (int s : spells) {
-            long long need = (success + s - 1) / s; 
-            int idx = lower_bound(potions.begin(), potions.end(), need) - potions.begin();
-            ans.push_back(m - idx);
+        for(int s : spells) {
+            long long need=(success+s-1)/s; 
+            int idx=lower_bound(potions.begin(), potions.end(), need) - potions.begin();
+            ans.push_back(m-idx);
         }
         return ans;
     }
