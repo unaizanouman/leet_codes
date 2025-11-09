0ecb6fd4124d618ac7b53ff671ff64c3762e359c
Unaiza Nouman <145625176+unaizanouman@users.noreply.github.com>
2025-11-09 20:58:46 +0500

    Time: 0 ms (100%), Space: 7.9 MB (71.74%) - LeetHub

---
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int operations = 0;
+        while (num1 != 0 && num2 != 0) {
+            if (num1 >= num2)
+                operations += num1 / num2, num1 %= num2;
+            else
+                operations += num2 / num1, num2 %= num1;
+        }
+        return operations;
+    }
+};
