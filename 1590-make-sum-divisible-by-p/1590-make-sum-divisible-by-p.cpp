643bc4f30f215035f3a697d1ae259515975aa068
Unaiza Nouman <145625176+unaizanouman@users.noreply.github.com>
2025-11-30 20:13:02 +0500

    Time: 75 ms (73.68%), Space: 87.7 MB (74.34%) - LeetHub

---
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) {
+        long long total = 0;
+        for (int x : nums) total += x;
+        int target = total % p;
+        if (target == 0) return 0;
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+        long long prefix = 0;
+        int ans = nums.size();
+        
+        for (int i = 0; i < nums.size(); i++) {
+            prefix = (prefix + nums[i]) % p;
+            
+            int need = (prefix - target + p) % p;
+            if (mp.count(need)) {
+                ans = min(ans, i - mp[need]);
+            }
+            
+            mp[prefix] = i;
+        }
+        
+        return ans == nums.size() ? -1 : ans;
+    }
+};
