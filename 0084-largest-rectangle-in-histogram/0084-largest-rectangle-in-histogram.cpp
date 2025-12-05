76e657c55146536b9577307abf47a384c66ab07d
Unaiza Nouman <145625176+unaizanouman@users.noreply.github.com>
2025-12-06 00:03:35 +0500

    Time: 24 ms (50.44%), Space: 81.4 MB (59.52%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -3,21 +3,19 @@ public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
         stack<int> st;
-        int maxArea = 0;
-
-        for (int i = 0; i <= n; i++) {
-            int h = (i == n ? 0 : heights[i]);
-
-            while (!st.empty() && h < heights[st.top()]) {
-                int height = heights[st.top()];
+        long long maxArea = 0;
+        heights.push_back(0);
+        for (int i = 0; i < heights.size(); i++) {
+            while (!st.empty() && heights[i] < heights[st.top()]) {
+                int h = heights[st.top()];
                 st.pop();
-                int width = st.empty() ? i : i - st.top() - 1;
-                maxArea = max(maxArea, height * width);
+                int right = i;
+                int left = st.empty() ? -1 : st.top();
+                long long width = right - left - 1;
+                maxArea = max(maxArea, width * h);
             }
-
             st.push(i);
         }
-
         return maxArea;
     }
 };
