1b5160615328e569cd441e52259b50d337396b33
Unaiza Nouman <145625176+unaizanouman@users.noreply.github.com>
2025-12-10 18:38:02 +0500

    Time: 0 ms (100%), Space: 12.6 MB (14.42%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -0,0 +1,16 @@
+class Solution:
+    def inorderTraversal(self, root):
+        result = []
+        stack = []
+        curr = root
+
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+
+            curr = stack.pop()
+            result.append(curr.val)
+            curr = curr.right
+
+        return result
