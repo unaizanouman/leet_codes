7a47b3af62e11a9f1c58ece1c33dd43d9b515678
Unaiza Nouman <145625176+unaizanouman@users.noreply.github.com>
2025-12-27 18:16:11 +0500

    Time: 21 ms (66.37%), Space: 46.1 MB (5.64%) - LeetHub

--- 1115-print-foobar-alternately/1115-print-foobar-alternately.java
+++ 1115-print-foobar-alternately/1115-print-foobar-alternately.java
@@ -2,25 +2,23 @@ import java.util.concurrent.Semaphore;
 
 class FooBar {
     private int n;
-    private Semaphore fooSem = new Semaphore(1); // foo starts first
+    private Semaphore fooSem = new Semaphore(1);
     private Semaphore barSem = new Semaphore(0);
 
     public FooBar(int n) {
         this.n = n;
     }
-
     public void foo(Runnable printFoo) throws InterruptedException {
         for (int i = 0; i < n; i++) {
             fooSem.acquire();
-            printFoo.run();   // prints "foo"
+            printFoo.run(); 
             barSem.release();
         }
     }
-
     public void bar(Runnable printBar) throws InterruptedException {
         for (int i = 0; i < n; i++) {
             barSem.acquire();
-            printBar.run();   // prints "bar"
+            printBar.run(); 
             fooSem.release();
         }
     }
