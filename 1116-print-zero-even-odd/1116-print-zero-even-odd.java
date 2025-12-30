a538dd13f15246fed2c8993c5911a5539b6e3f92
Unaiza Nouman <145625176+unaizanouman@users.noreply.github.com>
2025-12-30 21:08:21 +0500

    Time: 6 ms (20.94%), Space: 44.3 MB (14.5%) - LeetHub

---
+++ 1116-print-zero-even-odd/1116-print-zero-even-odd.java
@@ -0,0 +1,37 @@
+import java.util.concurrent.Semaphore;
+
+class ZeroEvenOdd {
+    private int n;
+    private Semaphore zero = new Semaphore(1);
+    private Semaphore even = new Semaphore(0);
+    private Semaphore odd = new Semaphore(0);
+
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            zero.acquire();
+            printNumber.accept(0);
+            if (i % 2 == 1) odd.release();
+            else even.release();
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i += 2) {
+            odd.acquire();
+            printNumber.accept(i);
+            zero.release();
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 2; i <= n; i += 2) {
+            even.acquire();
+            printNumber.accept(i);
+            zero.release();
+        }
+    }
+}
