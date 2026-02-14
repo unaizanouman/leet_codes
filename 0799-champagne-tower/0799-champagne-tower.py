a042f86fa06a8db802c0c261521bc5f297303c8d
Unaiza Nouman <145625176+unaizanouman@users.noreply.github.com>
2026-02-14 17:08:54 +0500

    Time: 53 ms (86.21%), Space: 12.5 MB (75.86%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.py
+++ 0799-champagne-tower/0799-champagne-tower.py
@@ -1,16 +1,12 @@
 class Solution(object):
     def champagneTower(self, poured, query_row, query_glass):
         row = [float(poured)]
-        
         for r in range(query_row):
             next_row = [0.0] * (len(row) + 1)
-            
             for i in range(len(row)):
                 if row[i] > 1:
                     overflow = (row[i] - 1) / 2.0
                     next_row[i] += overflow
                     next_row[i + 1] += overflow
-            
             row = next_row
-        
         return min(1, row[query_glass])
