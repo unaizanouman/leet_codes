715eed84124d9e49825c3c25dd16c1e72e651b3c
Unaiza Nouman <145625176+unaizanouman@users.noreply.github.com>
2026-02-16 16:05:13 +0500

    Time: 14 ms (78.54%), Space: 12.4 MB (54.55%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def reverseBits(self, n):
+        result = 0
+        for _ in range(32):
+            result=(result<<1) | (n&1)
+            n>>=1
+        return result
