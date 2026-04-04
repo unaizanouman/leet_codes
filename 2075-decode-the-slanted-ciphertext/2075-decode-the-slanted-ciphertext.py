cc1c8f187b031014ba6e5ffda83c311d565943a6
Unaiza Nouman <145625176+unaizanouman@users.noreply.github.com>
2026-04-04 18:11:26 +0500

    Time: 173 ms (60%), Space: 31.6 MB (20%) - LeetHub

--- 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.py
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.py
@@ -5,15 +5,11 @@ class Solution:
         
         n = len(encodedText)
         cols = n // rows
-        
-        # Step 1: build matrix
         matrix = []
         idx = 0
         for i in range(rows):
             matrix.append(list(encodedText[idx:idx+cols]))
             idx += cols
-        
-        # Step 2: read diagonals
         result = []
         
         for start_col in range(cols):
@@ -23,6 +19,4 @@ class Solution:
                 result.append(matrix[i][j])
                 i += 1
                 j += 1
-        
-        # Step 3: remove trailing spaces
         return "".join(result).rstrip()
