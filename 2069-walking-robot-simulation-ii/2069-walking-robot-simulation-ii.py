b6e6ba45c31462fda5aab2c51e533627628eb25e
Unaiza Nouman <145625176+unaizanouman@users.noreply.github.com>
2026-04-07 20:30:38 +0500

    Time: 192 ms (100%), Space: 17 MB (66.67%) - LeetHub

---
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.py
@@ -0,0 +1,54 @@
+class Robot:
+
+    def __init__(self, width, height):
+        self.w = width
+        self.h = height
+        
+        self.x = 0
+        self.y = 0
+        
+        self.dir = 0  # 0=East, 1=North, 2=West, 3=South
+        
+        self.perimeter = 2 * (width + height) - 4
+        self.moved = False  # to handle special case
+
+    def step(self, num):
+        self.moved = True
+        
+        num %= self.perimeter
+        
+        # special case: full cycle
+        if num == 0:
+            num = self.perimeter
+        
+        while num > 0:
+            if self.dir == 0:  # East
+                steps = min(num, self.w - 1 - self.x)
+                self.x += steps
+            elif self.dir == 1:  # North
+                steps = min(num, self.h - 1 - self.y)
+                self.y += steps
+            elif self.dir == 2:  # West
+                steps = min(num, self.x)
+                self.x -= steps
+            else:  # South
+                steps = min(num, self.y)
+                self.y -= steps
+            
+            num -= steps
+            
+            # if can't move further → turn
+            if num > 0:
+                self.dir = (self.dir + 1) % 4
+
+    def getPos(self):
+        return [self.x, self.y]
+
+    def getDir(self):
+        directions = ["East", "North", "West", "South"]
+        
+        # special case: never moved
+        if not self.moved:
+            return "East"
+        
+        return directions[self.dir]
