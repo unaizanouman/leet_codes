b7efbd1b2382b69f5ea60427267020281b848f1b
Unaiza Nouman <145625176+unaizanouman@users.noreply.github.com>
2026-04-11 16:29:39 +0500

    Time: 560 ms (68.75%), Space: 41.3 MB (43.75%) - LeetHub

--- 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.py
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.py
@@ -2,11 +2,14 @@ class Solution:
     def minimumDistance(self,nums):
         from collections import defaultdict
         pos=defaultdict(list)
+
         for i,v in enumerate(nums):
             pos[v].append(i)
         ans=float('inf')
+
         for arr in pos.values():
             if len(arr)>=3:
                 for i in range(len(arr)-2):
                     ans=min(ans,2*(arr[i+2]-arr[i]))
+                    
         return ans if ans!=float('inf') else -1
