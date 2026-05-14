acd1054037d7b25484a0a3a4def3a2cea2487aae
Unaiza <145625176+unaizanouman@users.noreply.github.com>
2026-05-14 21:43:01 +0500

    Time: 0 ms (100%), Space: 12.3 MB (96.23%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.py
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.py
@@ -1,17 +1,11 @@
 class Solution:
     def isGood(self, nums):
         n = max(nums)
-
-        # Length must be n + 1
         if len(nums) != n + 1:
             return False
 
         nums.sort()
-
-        # Check 1 to n-1 appear once
         for i in range(1, n):
             if nums[i - 1] != i:
                 return False
-
-        # Last two elements must both be n
         return nums[-1] == n and nums[-2] == n
